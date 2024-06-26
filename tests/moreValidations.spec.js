7a9e38b571a2db8adb214d37f646514ad963ef6e
ShubhamSarkarFrost <ronniecse8@gmail.com>
2024-06-26 22:49:54 +0530

    Playwright Code Update - 26-06-2024

---
+++ tests/moreValidations.spec.js
@@ -0,0 +1,61 @@
+const {test,expect} = require("@playwright/test");
+
+test('element display verification', async({page}) =>{
+    await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
+   
+    await page.locator("#displayed-text").scrollIntoViewIfNeeded();
+    await expect(page.locator("#displayed-text")).toBeVisible();
+    await page.locator("#hide-textbox").click();
+    await expect(page.locator("#displayed-text")).toBeHidden();
+    await page.locator("#show-textbox").click();
+    await expect(page.locator("#displayed-text")).toBeVisible();
+
+})
+
+test('handle page dialog', async({page}) =>{
+    await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
+   await page.locator("#confirmbtn").click();
+   page.on('dialog', dialog => dialog.accept());
+   await page.locator("#mousehover").hover();
+})
+
+test("@Web Popup validations",async({page})=>
+    {
+        await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
+    
+        // await page.goto("http://google.com");
+        // await page.goBack();
+        // await page.goForward();
+        await expect(page.locator("#displayed-text")).toBeVisible();
+        await page.locator("#hide-textbox").click();
+        await expect(page.locator("#displayed-text")).toBeHidden();
+       // await page.pause();
+        page.on('dialog',dialog => dialog.accept());
+        await page.locator("#confirmbtn").click();
+        await page.locator("#mousehover").hover();
+        const framesPage = page.frameLocator("#courses-iframe");
+        await framesPage.locator("li a[href*='lifetime-access']:visible").click();
+         const textCheck =await framesPage.locator(".text h2").textContent();
+        console.log(textCheck.split(" ")[1]);
+    
+    
+    })
+    
+    test("Screenshot & Visual comparision",async({page})=>
+    {
+        await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
+        await expect(page.locator("#displayed-text")).toBeVisible();
+        await page.locator('#displayed-text').screenshot({path:'partialScreenshot.png'});
+        await page.locator("#hide-textbox").click();
+        await page.screenshot({path: 'screenshot.png'});
+        await expect(page.locator("#displayed-text")).toBeHidden();
+    });
+    //screenshot -store -> screenshot -> 
+    test('visual',async({page})=>
+    {
+        //make payment -when you 0 balance
+          await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+        expect(await page.screenshot()).toMatchSnapshot('landing.png');
+    
+    })
+    
